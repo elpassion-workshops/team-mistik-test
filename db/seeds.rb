75e33eec152bafe2ba4d264933db4573b33714d4
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2016-09-09 20:59:05 +0200

    Better seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
-uri = URI('https://raw.githubusercontent.com/mlabouardy/Rated-Movies/master/client/data/top-rated.json')
-movies = Net::HTTP.get(uri)
-parsed_movies = JSON.parse(movies)['movies']
-parsed_movies.each do |movie|
-  # movie['img']
-  # movie['rating']
-  # movie['title']
-  # movie['year']
-  # movie['tags'].each do |tag|
-  #   tag['name']
-  # end
-  # movie['description']
+movies_json_url = URI('https://raw.githubusercontent.com/mlabouardy/Rated-Movies/master/client/data/top-rated.json')
+movies_json = Net::HTTP.get(movies_json_url)
+movies = JSON.parse(movies_json)['movies']
 
+Movie.destroy_all
 
+movies.each do |movie|
+  cover_url = movie['img']
+  rating = movie['rating'].to_i
+  title = movie['title']
+  year = movie['year'].to_i
+  tags = movie['tags'].map {|tag| tag['name']}
+  description = movie['description']
 
-  Movie.create!(title: movie['title'], description: movie['description'])
+  Movie.create!(title: title, description: description)
 end
