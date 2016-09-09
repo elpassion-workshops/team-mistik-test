70ce4bbbee5a4698646bd3473743ff29a41e2c2d
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2016-09-09 19:30:02 +0200

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+uri = URI('https://raw.githubusercontent.com/mlabouardy/Rated-Movies/master/client/data/top-rated.json')
+movies = Net::HTTP.get(uri)
+parsed_movies = JSON.parse(movies)['movies']
+parsed_movies.each do |movie|
+  # movie['img']
+  # movie['rating']
+  # movie['title']
+  # movie['year']
+  # movie['tags'].each do |tag|
+  #   tag['name']
+  # end
+  # movie['description']
+
+  Movie.create!(title: movie['title'], description: movie['description'])
+end
